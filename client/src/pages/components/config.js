640b9ac49659901749d7030bbdae0e2c86490a11
6EQUJ357 <abhi6equj5@gmail.com>
2023-08-24 23:39:32 +0530

    change  optimised build with server end point

--- client/src/pages/components/config.js
+++ client/src/pages/components/config.js
@@ -2,7 +2,7 @@
 //const API_BASE_URL = "http://localhost:8080"
    
   
-const API_BASE_URL = "https://ranga-invoice.onrender.com"
+const API_BASE_URL = "https://ranga-invoice-app.onrender.com"  
  
 
 
